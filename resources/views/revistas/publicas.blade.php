da1dfaa3a543f04904459a95bab02c7a6707106d
cmarrero2021 <marrero.c@gmail.com>
2024-08-29 22:35:09 -0400

    20240829_2233 Se agregó la página de revistas estática

---
+++ resources/views/revistas/publicas.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galería de Imágenes</title>
+    <link href="{{ asset('assets/css/styles_rev.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!--  <header>
+        <div class="header">
+            <div class="menu-container">
+                <a href="#" class="logo">Revistas</a>
+                <nav class="navbar">
+                    <ul>
+                     <li><a href="#">Inicio</a></li>
+                     <li><a href="#">Inicio</a></li>
+                     <li><a href="#">Inicio</a></li>
+                     <li><a href="#">Inicio</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+-->
+    <div class="revistas">
+        <div class="image-item">
+            <img src="{{asset('portadas/images/001. 5to. Objetivo.jpg')}}" alt="Descripción de la imagen 1">
+            <h3>Título de la imagen 1</h3>
+            <p>Descripción breve de la imagen 1.</p>
+            <span>Año: 2021</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/002. Academia.jpg')}}" alt="Descripción de la imagen 2">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/003. Acervo.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/004. Acta Bioclínica.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/005. Acta Científica.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/006. Acta Microscópica.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 1</h3>
+            <p>Descripción breve de la imagen 1.</p>
+            <span>Año: 2021</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/007. Actividad Física.jpg')}}" alt="Descripción de la imagen')}}">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/008. Actual.jpg')}}" alt="Descripción de la imagen')}}">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/009. Ágora Táctica.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/010. Agroecología Global.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/011. Agroindustria, Sociedad y Ambiente.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+        <div class="image-item">
+            <img src="{{asset('portadas/images/012. Akademos.jpg')}}" alt="Descripción de la imagen">
+            <h3>Título de la imagen 2</h3>
+            <p>Descripción breve de la imagen 2.</p>
+            <span>Año: 2022</span>
+        </div>
+    </div>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,8 +42,19 @@
                 </div>
             </div>
     </nav>
-    
-    <img src="{{ asset('vendor/adminlte/dist/img/logo_revista.png') }}" class="img-fluid w-25 mx-3 mt-3" alt="Logo Revista">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-3 mt-5">
+                <img src="{{ asset('vendor/adminlte/dist/img/logo_revista.png') }}" class="img-fluid w-100 mt-2" alt="Logo Revista">
+            </div>
+            <div class="col-sm-12 col-md-8 mt-4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan purus et placerat commodo. Nunc bibendum nulla sed ligula suscipit, quis vulputate nisl feugiat. Nam ac libero quis arcu placerat auctor. Sed cursus aliquet mauris a tincidunt. Mauris vehicula nisi ante, eget hendrerit metus aliquam a. Donec euismod sapien et fringilla eleifend. Sed aliquam, sem ut auctor elementum, mauris tellus fringilla neque, ut congue lectus dolor id est. Donec sed consectetur eros. Suspendisse dictum felis magna. Duis fringilla tincidunt dolor quis ornare. Suspendisse in massa ac nulla lobortis faucibus. Pellentesque gravida nibh a lacus congue, imperdiet porta purus scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div class="row">
+            <p>Aenean et gravida felis. Suspendisse vestibulum, ante ullamcorper facilisis congue, tellus turpis auctor justo, suscipit luctus sem elit et tortor. Aliquam convallis accumsan odio eget vehicula. Fusce lectus quam, maximus a commodo in, bibendum at tortor. Nulla ultricies at lacus et pulvinar. Aenean rhoncus nibh libero, id porta orci pharetra a. Morbi accumsan fringilla ligula, et condimentum turpis accumsan in. Fusce placerat dolor ac nulla tincidunt, ut mattis ex euismod. Nunc et nisl eget mauris congue finibus. </p>
+        </div>
+    </div>
 
     </body>
 </html>
